5100145a6abc027d93b379c14227f0ff411e1637
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-10-29 02:54:59 +0300

    Add project files.

---
+++ HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=HotelDB;integrated security=true;trusted_connection=true;encrypt=false");
+        }
+    }
+}
---
+++ HotelProject.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        Context context = new Context();
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
